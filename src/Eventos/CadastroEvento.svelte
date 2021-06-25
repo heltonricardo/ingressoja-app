1d5d8b0f535f74b399929ba053ade7a2380a94d2
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2021-06-25 20:10:18 -0300

    tela de cadastro de evento

--- src/Eventos/CadastroEvento.svelte
+++ src/Eventos/CadastroEvento.svelte
@@ -1,44 +1,49 @@
 <script>
   import { createEventDispatcher } from "svelte";
   import { postEvento } from "../Conexoes/eventoConex";
+  import Aguarde from "../UI/Aguarde.svelte";
   import Botao from "../UI/Botao.svelte";
   import Entrada from "../UI/Entrada.svelte";
 
   const dispatch = createEventDispatcher();
 
-  let titulo;
-  let descricao;
-  let imagemURL;
-  let online;
-  let inicio;
-  let termino;
-  let url;
-  let logradouro;
-  let numero;
-  let bairro;
-  let cidade;
-  let estado;
-  let pais;
+  let carregando = false;
+
+  let titulo = "";
+  let descricao = "";
+  let imagemURL = "";
+  let online = true;
+  let inicio = "";
+  let termino = "";
+  let url = "";
+  let logradouro = "";
+  let numero = "";
+  let bairro = "";
+  let cidade = "";
+  let estado = "";
+  let pais = "";
+  let cep = "";
 
   async function cadastrar() {
-    if (senha === senha2) {
-      const res = await postEvento({
-        titulo,
-        descricao,
-        imagemURL,
-        online,
-        inicio,
-        termino,
-        url,
-        logradouro,
-        numero,
-        bairro,
-        cidade,
-        estado,
-        pais,
-      });
-      if (res) dispatch("voltar");
-    }
+    carregando = true;
+    const res = await postEvento({
+      titulo,
+      descricao,
+      imagemURL,
+      online,
+      inicio,
+      termino,
+      url,
+      logradouro,
+      numero,
+      bairro,
+      cidade,
+      estado,
+      pais,
+      cep,
+    });
+    carregando = false;
+    if (res) dispatch("voltar");
   }
 </script>
 
@@ -50,10 +55,21 @@
     flex-direction: column;
   }
 
+  .campos {
+    display: flex;
+    flex-direction: column;
+  }
+
   h1 {
     font-size: 3rem;
     align-self: center;
-    margin-bottom: 4rem;
+    margin-bottom: 2rem;
+  }
+
+  #opcoes {
+    margin: 3rem 0;
+    display: flex;
+    justify-content: space-between;
   }
 
   #botoes {
@@ -63,38 +79,101 @@
   }
 </style>
 
-<div id="corpo">
-  <h1>Novo Evento</h1>
+{#if carregando}
+  <Aguarde />
+{/if}
 
-  <Entrada
-    id="titulo"
-    label="Título"
-    on:input={(event) => (titulo = event.target.value)}
-  />
-  <Entrada
-    id="imagemURL"
-    label="URL da Imagem de Capa"
-    on:input={(event) => (imagemURL = event.target.value)}
-  />
-  <Entrada
-    id="inicio"
-    label="Data e Hora de Início"
-    type="datetime-local"
-    on:input={(event) => (inicio = event.target.value)}
-  />
-  <Entrada
-    id="termino"
-    label="Data e Hora de Término"
-    type="datetime-local"
-    on:input={(event) => (termino = event.target.value)}
-  />
-  <Entrada
-    id="descricao"
-    label="Descrição"
-    controlType="textarea"
-    on:input={(event) => (termino = event.target.value)}
-  />
-  
+<div id="corpo">
+  <div class="campos">
+    <h1>Novo Evento</h1>
+    <Entrada
+      id="titulo"
+      label="Título"
+      on:input={(event) => (titulo = event.target.value)}
+    />
+    <Entrada
+      id="imagemURL"
+      label="URL da Imagem de Capa"
+      on:input={(event) => (imagemURL = event.target.value)}
+    />
+    <Entrada
+      id="inicio"
+      label="Data e Hora de Início"
+      type="datetime-local"
+      on:input={(event) => (inicio = event.target.value)}
+    />
+    <Entrada
+      id="termino"
+      label="Data e Hora de Término"
+      type="datetime-local"
+      on:input={(event) => (termino = event.target.value)}
+    />
+    <Entrada
+      id="descricao"
+      label="Descrição"
+      controlType="textarea"
+      on:input={(event) => (descricao = event.target.value)}
+    />
+  </div>
+
+  <div id="opcoes">
+    <label>
+      <input name="tipoEvento" type="radio" value={true} bind:group={online} />
+      Evento On-line
+    </label>
+
+    <label>
+      <input name="tipoEvento" type="radio" value={false} bind:group={online} />
+      Evento Presencial
+    </label>
+  </div>
+
+  <div class="campos">
+    {#if online}
+      <Entrada
+        id="url"
+        label="URL do Evento"
+        on:input={(event) => (url = event.target.value)}
+      />
+    {:else}
+      <Entrada
+        id="logradouro"
+        label="Logradouro"
+        on:input={(event) => (logradouro = event.target.value)}
+      />
+      <Entrada
+        id="numero"
+        label="Número"
+        type="number"
+        on:input={(event) => (numero = event.target.value)}
+      />
+      <Entrada
+        id="bairro"
+        label="Bairro"
+        on:input={(event) => (bairro = event.target.value)}
+      />
+      <Entrada
+        id="cidade"
+        label="Cidade"
+        on:input={(event) => (cidade = event.target.value)}
+      />
+      <Entrada
+        id="estado"
+        label="Estado"
+        on:input={(event) => (estado = event.target.value)}
+      />
+      <Entrada
+        id="pais"
+        label="País"
+        on:input={(event) => (pais = event.target.value)}
+      />
+      <Entrada
+        id="cep"
+        label="CEP"
+        on:input={(event) => (cep = event.target.value)}
+      />
+    {/if}
+  </div>
 </div>
 
 <div id="botoes">

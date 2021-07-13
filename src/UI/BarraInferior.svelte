aa75f022d611fae7f7da8033ec26562d085f95e7
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-07-12 23:05:05 -0300

    introduzindo informacoes na barra inferior

--- src/UI/BarraInferior.svelte
+++ src/UI/BarraInferior.svelte
@@ -1,14 +1,36 @@
-<div id="barra">
-
-</div>
-
 <style>
-    #barra {
+  #barra {
     width: 100%;
     height: 10rem;
+    text-align: center;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
     background-color: var(--verde1);
     -webkit-box-shadow: rgba(100, 100, 111, 0.2) 0 -7px 29px 0;
     -moz-box-shadow: rgba(100, 100, 111, 0.2) 0 -7px 29px 0;
     box-shadow: rgba(100, 100, 111, 0.2) 0 -7px 29px 0;
   }
+  span {
+    margin: 0.3rem;
+    color: var(--roxo2);
+  }
+
+  span a {
+    font-weight: bolder;
+    text-decoration: none;
+    color: var(--roxo2);
+  }
 </style>
+
+<div id="barra">
+  <span>© 2021, Plataforma IngressoJá! Todos os direitos reservados.</span>
+  <span
+    ><a target="_blank" href="https://github.com/heltonricardo"
+      >Entre em Contato</a
+    ></span
+  >
+</div>

ecb7bf6f4f72ebe058b53d0744f7c2dbe9f21050
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-07-17 16:25:53 -0300

    refatorando codigo de estilo da tela meus dados

--- src/Conta/MeusDados.svelte
+++ src/Conta/MeusDados.svelte
@@ -55,25 +55,10 @@
     margin: 1rem 0;
   }
 
-  #conteudo {
-    width: 100%;
-    margin: 1rem 0;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .linha {
-    display: flex;
-  }
-
   .titulo {
     font-weight: bold;
-    margin-right: 0.1rem;
-    min-width: 9rem;
-  }
-
-  p {
-    margin: 0.8rem 0;
+    white-space: nowrap;
+    padding: 1rem;
   }
 
   #botoes {
@@ -89,152 +74,86 @@
   {:then dados}
     <div id="conteudo">
       {#if tipoLogado === TIPOCADASTRO.COMPRADOR}
-        <div class="linha">
-          <div class="titulo">
-            <p>Tipo:</p>
-          </div>
-          <div class="dados">
-            <p>Comprador</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>Id:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.id}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>Nome:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.nome}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>E-mail:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.email}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>CPF:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.cpf}</p>
-          </div>
-        </div>
+        <table>
+          <tr>
+            <td class="titulo">Tipo:</td>
+            <td class="dado">Comprador</td>
+          </tr>
+          <tr>
+            <td class="titulo">Id:</td>
+            <td class="dado">{dados.id}</td>
+          </tr>
+          <tr>
+            <td class="titulo">Nome:</td>
+            <td class="dado">{dados.nome}</td>
+          </tr>
+          <tr>
+            <td class="titulo">E-mail:</td>
+            <td class="dado">{dados.email}</td>
+          </tr>
+          <tr>
+            <td class="titulo">CPF:</td>
+            <td class="dado">{dados.cpf}</td>
+          </tr>
+        </table>
       {:else if tipoLogado === TIPOCADASTRO.PRODUTORA}
-        <div class="linha">
-          <div class="titulo">
-            <p>Tipo:</p>
-          </div>
-          <div class="dados">
-            <p>Produtora</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>Id:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.id}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>Razão social:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.razaoSocial}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>Nome fantasia:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.nomeFantasia}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>CNPJ:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.cnpj}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>E-mail:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.email}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>Banco:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.banco}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>Agência:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.agencia}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>Conta:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.conta}</p>
-          </div>
-        </div>
+        <table>
+          <tr>
+            <td class="titulo">Tipo:</td>
+            <td class="dado">Produtora</td>
+          </tr>
+          <tr>
+            <td class="titulo">Id:</td>
+            <td class="dado">{dados.id}</td>
+          </tr>
+          <tr>
+            <td class="titulo">Razão social:</td>
+            <td class="dado">{dados.razaoSocial}</td>
+          </tr>
+          <tr>
+            <td class="titulo">Nome fantasia:</td>
+            <td class="dado">{dados.nomeFantasia}</td>
+          </tr>
+          <tr>
+            <td class="titulo">CNPJ:</td>
+            <td class="dado">{dados.cnpj}</td>
+          </tr>
+          <tr>
+            <td class="titulo">E-mail:</td>
+            <td class="dado">{dados.email}</td>
+          </tr>
+          <tr>
+            <td class="titulo">Banco:</td>
+            <td class="dado">{dados.banco}</td>
+          </tr>
+          <tr>
+            <td class="titulo">Agência:</td>
+            <td class="dado">{dados.agencia}</td>
+          </tr>
+          <tr>
+            <td class="titulo">Conta:</td>
+            <td class="dado">{dados.conta}</td>
+          </tr>
+        </table>
       {:else if tipoLogado === TIPOCADASTRO.ADMINISTRADOR}
-        <div class="linha">
-          <div class="titulo">
-            <p>Tipo:</p>
-          </div>
-          <div class="dados">
-            <p>Administrador</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>Id:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.id}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>Nome:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.nome}</p>
-          </div>
-        </div>
-        <div class="linha">
-          <div class="titulo">
-            <p>E-mail:</p>
-          </div>
-          <div class="dados">
-            <p>{dados.email}</p>
-          </div>
-        </div>
+        <table>
+          <tr>
+            <td class="titulo">Tipo:</td>
+            <td class="dado">Administrador</td>
+          </tr>
+          <tr>
+            <td class="titulo">Id:</td>
+            <td class="dado">{dados.id}</td>
+          </tr>
+          <tr>
+            <td class="titulo">Nome:</td>
+            <td class="dado">{dados.nome}</td>
+          </tr>
+          <tr>
+            <td class="titulo">E-mail:</td>
+            <td class="dado">{dados.email}</td>
+          </tr>
+        </table>
       {/if}
     </div>
   {/await}

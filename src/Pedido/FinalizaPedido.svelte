a68586de04f3553fdb5df73cd7815a834e3a3328
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-10-20 21:29:18 -0300

    preparando tela de finalizacao de pedido

--- src/Pedido/FinalizaPedido.svelte
+++ src/Pedido/FinalizaPedido.svelte
@@ -1,16 +1,13 @@
 <script>
   import Swal from "sweetalert2";
-  import validator from "validator";
-  import { validateBr } from "js-brasil";
   import { createEventDispatcher } from "svelte";
 
   import MSG from "../ENUM/MSG";
   import Botao from "../UI/Botao.svelte";
-  import Entrada from "../UI/Entrada.svelte";
   import Aguarde from "../UI/Aguarde.svelte";
   import CardIngresso from "./CardIngresso.svelte";
   import { postPedido } from "../Conexao/pedidoConex";
-  import { onlyLetrasEEspacos, onlyNumeros } from "../utils/sanitarizador";
+  import { onlyNumeros } from "../utils/sanitarizador";
 
   const dispatch = createEventDispatcher();
 
@@ -49,10 +46,6 @@
   async function concluir() {
     carregando = true;
 
-    pedido.numeroCartao = onlyNumeros(pedido.numeroCartao);
-    pedido.codigoCartao = onlyNumeros(pedido.codigoCartao);
-    pedido.cpfTitular = onlyNumeros(pedido.cpfTitular);
-
     let itensPedido = ingressos.map((i) => ({
       idTipoDeIngresso: i.id,
       ingressante: i.ingressante,
@@ -70,26 +63,7 @@
 
   /*************************** VALIDAÇÃO DE CAMPOS ****************************/
 
-  pedido.numeroCartao = "";
-  pedido.codigoCartao = "";
-  pedido.nomeTitular = "";
-  pedido.cpfTitular = "";
-
-  $: numeroCartaoValido = validator.isCreditCard(pedido.numeroCartao);
-  $: codigoCartaoValido =
-    validator.isLength(pedido.codigoCartao.trim(), { min: 3, max: 4 }) &&
-    validator.isNumeric(pedido.codigoCartao);
-  $: nomeTitularValido =
-    validator.isLength(pedido.nomeTitular.trim(), { min: 1, max: 255 }) &&
-    validator.isAlpha(pedido.nomeTitular, "pt-BR", { ignore: " " });
-  $: cpfTitularValido = validateBr.cpf(pedido.cpfTitular);
-
-  $: formularioValido =
-    numeroCartaoValido &&
-    codigoCartaoValido &&
-    nomeTitularValido &&
-    cpfTitularValido &&
-    ingressos.every((i) => i.valido);
+  $: formularioValido = ingressos.every((i) => i.valido);
 </script>
 
 <style>
@@ -120,7 +94,8 @@
   #botoes {
     margin: 3rem 0;
     display: flex;
-    justify-content: space-around;
+    flex-direction: column;
+    align-items: center;
   }
 </style>
 
@@ -136,45 +111,10 @@
     <CardIngresso bind:ingresso={ingressos[x]} />
   {/each}
 
-  <h1 id="titulo">Detalhes de Pagamento</h1>
-  <h3 id="subtitulo">Insira os dados do cartão de crédito:</h3>
-
-  <Entrada
-    id="numeroCartao"
-    label="Número do cartão"
-    on:input={(event) => (pedido.numeroCartao = event.target.value)}
-    valido={numeroCartaoValido}
-    mensagemValidacao="Insira um número de cartão de crédito válido"
-    maxlength="19"
-  />
-  <Entrada
-    id="codigoCartao"
-    label="Código de segurança"
-    type="number"
-    on:input={(event) => (pedido.codigoCartao = event.target.value)}
-    valido={codigoCartaoValido}
-    mensagemValidacao="Insira um código de segurança válido"
-    maxlength="4"
-  />
-  <Entrada
-    id="nomeTitular"
-    label="Titular (como consta no cartão)"
-    on:input={(event) => (pedido.nomeTitular = event.target.value)}
-    valido={nomeTitularValido}
-    mensagemValidacao="Insira um titular válido"
-  />
-  <Entrada
-    id="cpfTitular"
-    label="CPF do titular"
-    on:input={(event) => (pedido.cpfTitular = event.target.value)}
-    valido={cpfTitularValido}
-    mensagemValidacao="Insira um CPF válido"
-  />
-
   <div id="botoes">
-    <Botao on:click={voltar}>Página principal</Botao>
     <Botao on:click={concluir} habilitado={formularioValido}
-      >Concluir pedido</Botao
+      >Prosseguir para pagamento</Botao
     >
+    <Botao on:click={voltar}>Cancelar</Botao>
   </div>
 </div>

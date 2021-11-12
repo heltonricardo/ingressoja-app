66c6b030ff2f68b8dc63486af1b60bf852307f4b
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-11-12 18:11:02 -0300

    alternando tipos de grafico

--- src/UI/Grafico.svelte
+++ src/UI/Grafico.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import Chart from "chart.js/auto";
+  import TIPOGRAFICO from "../ENUM/TIPOGRAFICO";
 
   export let titulo = "";
   export let dados = null;
@@ -8,7 +9,10 @@
   export let valor;
 
   let canvas;
+  let limitarAltura = true;
+  let grafico;
   let tamanho = dados?.length | 0;
+  let tipoGrafico = TIPOGRAFICO.PIE;
 
   let cores = [...Array(tamanho)].map(
     () =>
@@ -18,17 +22,41 @@
         .join("")
   );
 
+  let graficoConfig = {
+    type: tipoGrafico,
+    data: {
+      labels: dados.map((d) => d[legenda]),
+      datasets: [{ backgroundColor: cores, data: dados.map((d) => d[valor]) }],
+    },
+  };
+
   onMount(() => {
-    new Chart(canvas, {
-      type: "doughnut",
-      data: {
-        labels: dados.map((d) => d[legenda]),
-        datasets: [
-          { backgroundColor: cores, data: dados.map((d) => d[valor]) },
-        ],
-      },
-    });
+    grafico = new Chart(canvas, graficoConfig);
   });
+
+  function trocarGrafico() {
+    if (tipoGrafico === TIPOGRAFICO.BARRAS) {
+      limitarAltura = false;
+      graficoConfig = {
+        ...graficoConfig,
+        type: tipoGrafico,
+        options: {
+          indexAxis: "y",
+          legend: { display: false },
+        },
+      };
+    } else {
+      limitarAltura = true;
+      graficoConfig = {
+        ...graficoConfig,
+        type: tipoGrafico,
+        options: {},
+      };
+    }
+
+    grafico.destroy();
+    grafico = new Chart(canvas, graficoConfig);
+  }
 </script>
 
 <style>
@@ -40,12 +68,35 @@
     margin: 4rem 0;
   }
 
-  canvas {
+  h3 {
+    font-size: 1.1rem;
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    font-size: 0.8rem;
+    color: var(--cinza3);
+  }
+
+  .alturaMax {
     max-height: 30rem;
   }
 </style>
 
 <div class="corpo">
   <h3>{titulo}</h3>
-  <canvas bind:this={canvas} />
+  <p class="tipo">
+    Tipo de gráfico: <span>
+      <select bind:value={tipoGrafico} on:change={trocarGrafico}>
+        <option value={TIPOGRAFICO.PIE} on:click={trocarGrafico}>Pizza</option>
+        <option value={TIPOGRAFICO.DOUGHNUT} on:click={trocarGrafico}
+          >Rosca</option
+        >
+        <option value={TIPOGRAFICO.BARRAS} on:click={trocarGrafico}
+          >Barras</option
+        >
+      </select>
+    </span>
+  </p>
+  <canvas class:alturaMax={limitarAltura} bind:this={canvas} />
 </div>

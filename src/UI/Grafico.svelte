ed55c38b82e3579340d74792c3c63dc1590705a4
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-11-14 22:36:59 -0300

    opcao de somente grafico barras

--- src/UI/Grafico.svelte
+++ src/UI/Grafico.svelte
@@ -3,16 +3,17 @@
   import Chart from "chart.js/auto";
   import TIPOGRAFICO from "../ENUM/TIPOGRAFICO";
 
-  export let titulo = "";
-  export let dados = null;
-  export let legenda;
   export let valor;
+  export let legenda;
+  export let dados = null;
+  export let somenteBarras = false;
 
   let canvas;
-  let limitarAltura = true;
   let grafico;
   let tamanho = dados?.length | 0;
-  let tipoGrafico = TIPOGRAFICO.DOUGHNUT;
+  let tipoGrafico = somenteBarras ? TIPOGRAFICO.BARRAS : TIPOGRAFICO.DOUGHNUT;
+
+  $: limitarAltura = tipoGrafico !== TIPOGRAFICO.BARRAS;
 
   let cores = [...Array(tamanho)].map(
     () =>
@@ -22,32 +23,50 @@
         .join("")
   );
 
-  let graficoConfig = {
-    type: tipoGrafico,
-    data: {
-      labels: dados.map((d) => d[legenda]),
-      datasets: [{ backgroundColor: cores, data: dados.map((d) => d[valor]) }],
-    },
-  };
+  let graficoConfig =
+    tipoGrafico === TIPOGRAFICO.BARRAS
+      ? {
+          type: tipoGrafico,
+          data: {
+            labels: dados.map((d) => d[legenda]),
+            datasets: [
+              { backgroundColor: cores, data: dados.map((d) => d[valor]) },
+            ],
+          },
+          options: {
+            indexAxis: "y",
+            plugins: { legend: { display: false } },
+          },
+        }
+      : {
+          type: tipoGrafico,
+          data: {
+            labels: dados.map((d) => d[legenda]),
+            datasets: [
+              { backgroundColor: cores, data: dados.map((d) => d[valor]) },
+            ],
+          },
+        };
 
   onMount(() => (grafico = new Chart(canvas, graficoConfig)));
 
   function trocarGrafico() {
-    if (tipoGrafico === TIPOGRAFICO.BARRAS) {
-      limitarAltura = false;
-      graficoConfig = {
-        ...graficoConfig,
-        type: tipoGrafico,
-        options: { indexAxis: "y", plugins: { legend: { display: false } } },
-      };
-    } else {
-      limitarAltura = true;
-      graficoConfig = {
-        ...graficoConfig,
-        type: tipoGrafico,
-        options: null,
-      };
-    }
+    graficoConfig =
+      tipoGrafico === TIPOGRAFICO.BARRAS
+        ? {
+            ...graficoConfig,
+            type: tipoGrafico,
+            options: {
+              indexAxis: "y",
+              plugins: { legend: { display: false } },
+            },
+          }
+        : {
+            ...graficoConfig,
+            type: tipoGrafico,
+            options: null,
+          };
+
     grafico.destroy();
     grafico = new Chart(canvas, graficoConfig);
   }
@@ -59,11 +78,6 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-  }
-
-  h3 {
-    font-size: 1.1rem;
-    margin-bottom: 0.5rem;
   }
 
   p {
@@ -83,14 +97,16 @@
 </style>
 
 <div class="corpo">
-  <h3>{titulo}</h3>
   <p class="tipo">
     Tipo de gráfico: <span>
       <select bind:value={tipoGrafico} on:change={trocarGrafico}>
-        <option value={TIPOGRAFICO.PIE} on:click={trocarGrafico}>Pizza</option>
-        <option value={TIPOGRAFICO.DOUGHNUT} on:click={trocarGrafico}
-          >Rosca</option
-        >
+        {#if !somenteBarras}
+          <option value={TIPOGRAFICO.PIE} on:click={trocarGrafico}>Pizza</option
+          >
+          <option value={TIPOGRAFICO.DOUGHNUT} on:click={trocarGrafico}
+            >Rosca</option
+          >
+        {/if}
         <option value={TIPOGRAFICO.BARRAS} on:click={trocarGrafico}
           >Barras</option
         >
